02f2854b6520b7c7823e38a7864d617023e791e1
liron2704 <liron.lironl3@gmail.com>
2024-12-10 15:05:41 +0200

    student_course1

--- studen_course_ex/Course.py
+++ studen_course_ex/Course.py
@@ -1,4 +1,8 @@
 class Course:
 
-    def __init__(self,course_id,course_name,subject_teacher_dic,student):
-        pass
+    def __init__(self,course_id,course_name,max_student):
+        self.subject_teacher_dic = {}
+        self.student_list = []
+        self.course_id = course_id
+        self.course_name = course_name
+        self.max_student = max_student
