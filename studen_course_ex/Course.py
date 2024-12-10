1e324d30715c698ca955c5db6448acac4d91a5b8
liron2704 <liron.lironl3@gmail.com>
2024-12-10 17:16:23 +0200

    student_course2

--- studen_course_ex/Course.py
+++ studen_course_ex/Course.py
@@ -14,8 +14,16 @@ class Course:
         return (f"course number: {self.course_id}\n"
                 f"course name: {self.course_name}\n"
                 f"max student in course: {self.max_student}\n"
-                f"student list: \n{self.student_list}"
+                f"student list: \n{self.student_list}\n"
                 f"subject and teachers: {self.subject_teacher_dic}")
+
+    def add_sub_teacher(self,subject_name,teacher):
+        self.subject_teacher_dic[subject_name]=teacher
+
+    def space_in_course(self):
+        if len(self.student_list)+1>self.max_student:
+            return False
+        return True
 
     def add_student(self,student:Student):
         if len(self.student_list)+1 > self.max_student:
@@ -28,4 +36,37 @@ class Course:
     def add_factor(self,subject_name,factor):
         for student in self.student_list:
             student.calc_factor(subject_name,factor)
+
+    def del_student(self,student:Student):
+        if student not in self.student_list:
+            print("Student not in course!!")
+            return
+        self.student_list.remove(student)
+
+    def averages(self):
+        student_avg_dic={}
+        for student in self.student_list:
+            student_avg_dic[student.i_d] = student.average()
+        return student_avg_dic
+
+    def weak_students(self):
+        lowest_grades=[]
+        averages = self.averages()  # Assuming this returns a dictionary {id: grade}
+        if not averages:
+            return []  # Handle the case where there are no grades
+
+        # Find the minimum grade
+        lowest_grade_value = min(averages.values())
+
+        # Collect the IDs of students with the lowest grade
+        #lowest_grades = [student_id for student_id, grade in averages.items() if grade == lowest_grade_value]
+        for student_id,grade in averages.items():
+            if grade == lowest_grade_value:
+                lowest_grades.append(student_id)
+
+        return lowest_grades
+
+
+
+
 
--- studen_course_ex/main.py
+++ studen_course_ex/main.py
@@ -1,17 +1,39 @@
 from studen_course_ex.Course import Course
 from studen_course_ex.Student import Student
 
-liron = Student('208233650','Liron Lojkin',25)
-moshe = Student('208233655','Moshe Cohen',70)
-liron.add_grade('python',70)
-liron.add_grade('qa',90)
-moshe.add_grade('python',80)
-python_course = Course('1234','python',5)
-python_course.add_student(liron)
-python_course.add_student(moshe)
-python_course.add_factor('python',10)
-print(liron.subject_grade_dic)
-print(moshe.subject_grade_dic)
+course_number = input("Please enter course number: ")
+course_name = input("Please enter course name: ")
+max_students = int(input("Please enter max students in course: "))
+course1 = Course(course_number,course_name,max_students)
+subject1 = input("Please enter subject: ")
+teacher1 = input("Please enter teacher name: ")
+course1.add_sub_teacher(subject1,teacher1)
+student_id = input("enter student id: ")
+
+while student_id != '0':
+    if not course1.space_in_course():
+        break
+    student_name = input("enter student name: ")
+    subject_name = input("enter subject name: ")
+    grade = int(input("enter subject name: "))
+
+
+    student_id = input("enter student id: ")
+
+
+
+# liron = Student('208233650','Liron Lojkin',25)
+# moshe = Student('312456345','Moshe Cohen',70)
+# liron.add_grade('python',70)
+# liron.add_grade('qa',90)
+# moshe.add_grade('python',80)
+# python_course = Course('1234','python',5)
+# python_course.add_student(liron)
+# python_course.add_student(moshe)
+# #python_course.add_factor('python',10)
+# print(python_course.weak_students())
+
+
 
 
 
